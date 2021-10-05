7369b3df8ce1d237060efa2224d949771a5e9f7f
Maks <Delaerous12@yandex.ru>
2021-10-05 21:57:17 +0300

    newCarousel

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -1,12 +1,13 @@
-$(document).ready(function(){
-	$('.latest-products__carousel').bxSlider({
-		pager: false,
-		controls: true,
-		minSlides: 5,
-		maxSlides: 5,
-		slideMargin: 20,        
-		slideWidth: 236
-	});
+$(function() {
+    // Owl Carousel
+    let owl = $(".owl-carousel");
+    owl.owlCarousel({
+      items: 3,
+      margin: 10,
+      loop: true,
+      nav: true,
+      dots: false,
+    });
 	$('.carousel').bxSlider({
 		pager: false,
 		controls: true,
@@ -15,4 +16,4 @@ $(document).ready(function(){
 		slideMargin: 20,        
 		slideWidth: 236
 	});
-})
+  });
