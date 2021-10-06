81e6566d3f94ee99c580d9e8ae245aa5dfa4266b
Maks <Delaerous12@yandex.ru>
2021-10-06 15:46:34 +0300

    new carousel with hover

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -2,11 +2,16 @@ $(function() {
     // Owl Carousel
     let owl = $(".owl-carousel");
     owl.owlCarousel({
-      items: 3,
+      items: 5,
       margin: 10,
       loop: true,
       nav: true,
+      navText: [
+        '<div class="carousel__prev" aria-hidden="true">Prev</div>',
+        '<div class="carousel__next" aria-hidden="true">Next</div>'
+    ],
       dots: false,
+      
     });
 	$('.carousel').bxSlider({
 		pager: false,
--- src/scripts/cart.js
+++ src/scripts/cart.js
@@ -1,5 +1,19 @@
-var clicks = 0;
+var clicks = localStorage.getItem('clicks') || 0;
+    document.addEventListener("DOMContentLoaded", function(){ document.getElementById("clicks").innerHTML = localStorage.getItem('clicks') || 0; });
     function onClick() {
-        clicks += 1;
+      clicks = +clicks + 1;
+      document.getElementById("clicks").innerHTML = clicks;
+      updateStorage(clicks);
+    };
+
+    function onReset() {
+      if (confirm('Вы уверенны? Данные обнулятся!')) {
+        clicks = 0;
         document.getElementById("clicks").innerHTML = clicks;
+        updateStorage(clicks);
+      }
+    }
+
+    function updateStorage(clicks) {
+      localStorage.setItem('clicks', clicks);
     }
